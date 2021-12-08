7130f68a273e323b58788dd9ef6430d830651a6b
WA1RCT <95482615+WA1RCT@users.noreply.github.com>
2021-12-08 13:31:00 -0500

    Add files via upload

--- ATU_100_EXT_board/FirmWare_PIC16F1938/1938_EXT_MPLAB_sources_V_3.2/main.c
+++ ATU_100_EXT_board/FirmWare_PIC16F1938/1938_EXT_MPLAB_sources_V_3.2/main.c
@@ -45,6 +45,72 @@ char g_b_tune_btn_released;
 
 void main()
 {
+#ifdef __DEBUG    
+    /*  print in debug mode */ 
+    char mystart[] = "Starting!";
+    PRINTLINE(mystart)  
+    eeprom_write(EEPROM_DISPLAY_I2C_ADDR,0x4E);
+    eeprom_write(EEPROM_DISPLAY_TYPE,4);
+    eeprom_write(EEPROM_AUTOMATIC_MODE,0);
+    eeprom_write(EEPROM_TIMEOUT_TIME,0x15);
+    eeprom_write(EEPROM_SWR_THRESHOLD,0x13);
+    eeprom_write(EEPROM_MIN_POWER,5);
+    eeprom_write(EEPROM_MAX_POWER,0);
+    eeprom_write(EEPROM_DISPLAY_OFFSET_DOWN,2);
+    eeprom_write(EEPROM_DISPLAY_OFFSET_LEFT,3);
+    eeprom_write(EEPROM_MAX_INIT_SWR,0);
+    eeprom_write(EEPROM_NUMBER_INDS,7);
+    eeprom_write(EEPROM_IND_LINEAR_PITCH,0);
+    eeprom_write(EEPROM_NUMBER_CAPS,7);
+    eeprom_write(EEPROM_CAP_LINEAR_PITCH,0);
+    eeprom_write(EEPROM_ENABLE_NONLINEAR_DIODE,1);
+    eeprom_write(EEPROM_INVERSE_INDUCTANCE_RELAY,0);
+    
+    eeprom_write(0x10,0);    
+    eeprom_write(0x11,0x50);    
+    eeprom_write(0x12,1);    
+    eeprom_write(0x13,0x10);    
+    eeprom_write(0x14,2);    
+    eeprom_write(0x15,0x20);    
+    eeprom_write(0x16,4);    
+    eeprom_write(0x17,0x50);    
+    eeprom_write(0x18,0x10);    
+    eeprom_write(0x19,0);    
+    eeprom_write(0x1a,0x22);    
+    eeprom_write(0x1b,0);    
+    eeprom_write(0x1c,0x45);    
+    eeprom_write(0x1d,0);    
+ 
+    eeprom_write(0x20,0);    
+    eeprom_write(0x21,0x10);    
+    eeprom_write(0x22,0);    
+    eeprom_write(0x23,0x22);    
+    eeprom_write(0x24,0);    
+    eeprom_write(0x25,0x47);    
+    eeprom_write(0x26,1);    
+    eeprom_write(0x27,0x00);    
+    eeprom_write(0x28,2);    
+    eeprom_write(0x29,0x20);    
+    eeprom_write(0x2a,4);    
+    eeprom_write(0x2b,0x70);    
+    eeprom_write(0x2c,0x10);    
+    eeprom_write(0x2d,0);    
+    
+    
+    eeprom_write(EEPROM_POWER_MEASURE_LEVEL,0);
+    eeprom_write(EEPROM_TANDEM_MATCH,10);
+    eeprom_write(EEPROM_DISPLAY_OFF_TIMER,0);
+    eeprom_write(EEPROM_ADDITIONAL_INDICATION,1);
+    eeprom_write(EEPROM_FEEDER_LOSS,0x12);
+    eeprom_write(EEPROM_DISABLE_RELAYS,0x0);
+    eeprom_write(EEPROM_LAST_SWR_L,0x18);
+    eeprom_write(EEPROM_LAST_SWR_H,0);
+    eeprom_write(EEPROM_LAST_SW,0);
+    eeprom_write(EEPROM_LAST_IND,0);
+    eeprom_write(EEPROM_LAST_CAP,0);
+    
+ #endif
+           
    if (STATUSbits.nTO == 0)
       g_b_Restart = 1;
    pic_init();
